6da03e7e6afbed21e1b6d33527d8cdaf7c59a714
KJ Monahan <kejm42@gmail.com>
2024-11-13 19:17:21 -0600

    Move front-end to a separate project

--- src/main/java/dev/kjmonahan/todo/app/src/routes/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async ({ fetch }) => {
-    const response = await fetch(`/api/next-action`);
-    const actions = await response.json();
-    return {actions}
-};
